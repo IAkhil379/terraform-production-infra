aa32036a0bad511c43723c157972fa3c125d4162
I AKHIL <akhilelijah3@gmail.com>
2026-06-22 23:50:48 +0530

    Configure Application Load Balancer and Target Group health checks

---
+++ alb.tf
@@ -0,0 +1,35 @@
+resource "aws_lb" "prod_alb" {
+  name               = "prod-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+}
+
+resource "aws_lb_target_group" "prod_tg" {
+  name     = "prod-web-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.prod_vpc.id
+
+  health_check {
+    path                = "/"
+    port                = "80"
+    protocol            = "HTTP"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.prod_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.prod_tg.arn
+  }
+}
